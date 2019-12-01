c13f8359f9852fd4e70dc5e905f9f413b420b00c
Felix Schulze Sindern <felixs@fh-muenster.de>
2019-12-01 23:11:14 +0100

    improve import of secrets to allow compatibility across mulitple competitions

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import secrets
 import scrape
 import telegram
 import asyncio
@@ -9,30 +8,35 @@ import sys
 
 hosts = ['streamja', 'streamable', 'imgtc', 'clippituser', 'vimeo', 'streamvi']
 
+
 async def main():
-    try: competition = setup.competition(sys.argv[1])
-    except IndexError: competition = setup.competition('buli')
+    try:
+        competition = setup.competition(sys.argv[1])
+        chat_id = setup.chat_id(sys.argv[1])
+    except IndexError:
+        competition = setup.competition('buli')
+        chat_id = setup.chat_id('buli')
 
     bot = setup.telegramBot()
     subreddit = setup.redditBot()
 
     try:
-        # for submission in subreddit.stream.submissions():
-        #     await process_submission(submission, bot, competition)
+        for submission in subreddit.stream.submissions():
+            await process_submission(submission, bot, competition, chat_id)
 
         # Use this for testing!
-        submissions = subreddit.new(limit=200)
-        for submission in submissions:
-            await process_submission(submission, bot, competition)
+        # submissions = subreddit.new(limit=200)
+        # for submission in submissions:
+        #     await process_submission(submission, bot, competition, chat_id)
 
     except KeyboardInterrupt:
-        print('CTRL + C detected. closing...')
+        print(bcolors.FAIL + 'CTRL + C detected. closing...' + bcolors.ENDC)
         quit()
     except:
         print(bcolors.FAIL + 'crashed.' + bcolors.ENDC)
 
 
-async def process_submission(submission, bot, competition):
+async def process_submission(submission, bot, competition, chat_id):
     normalized_title = submission.title.lower().split()
     text = '<a href="{}">{}</a>'.format(submission.url, submission.title)
     if filter(normalized_title, submission.url, submission.created_utc, competition):
@@ -40,21 +44,19 @@ async def process_submission(submission, bot, competition):
         try:
             mp4Link = await scrape.mp4Link(submission.url)
             if mp4Link:
-                bot.send_video(chat_id=secrets.telegram_chat_id, caption=submission.title,
+                bot.send_video(chat_id=chat_id, caption=submission.title,
                                video=mp4Link)
                 print('Successfully scraped mp4 link. Sening video...')
             else:
                 print(bcolors.WARNING +
                       'Couldnt scrape mp4 link. Sening link...' + bcolors.ENDC)
-                bot.send_message(chat_id=secrets.telegram_chat_id,
+                bot.send_message(chat_id=chat_id,
                                  text=text, parse_mode=telegram.ParseMode.HTML)
         except Exception as e:
             print(bcolors.FAIL + 'Exception occured: ' + str(e) + bcolors.ENDC)
-            bot.send_message(chat_id=secrets.telegram_chat_id,
+            bot.send_message(chat_id=chat_id,
                              text='Whoops! Something went wrong when scraping this URL: ' + submission.url)
-            print(
-                'Whoops! Something went wrong when scraping this URL: ' + submission.url)
-            bot.send_message(chat_id=secrets.telegram_chat_id,
+            bot.send_message(chat_id=chat_id,
                              text=text, parse_mode=telegram.ParseMode.HTML)
 
 
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import praw
 import secrets
 from competition import Competition, Team
 import telegram
+from bcolors import bcolors
 
 buli = [['gladbach', 'mönchengladbach', 'monchengladbach'], ['leipzig'], ['bayern', 'münchen', 'munchen', 'munich'], ['freiburg'], ['hoffenheim'], ['dortmund'], ['schalke'], ['leverkusen', 'bayer'],
         ['frankfurt'], ['wolfsburg'], ['union'], ['hertha'], ['düsseldorf'], ['dusseldorf'], ['werder', 'bremen'], ['augsburg'], ['mainz'], ['köln', 'koln', 'cologne'], ['paderborn']]
@@ -27,18 +28,26 @@ def telegramBot():
 def competition(competition):
     teams = []
     if competition == 'buli':
+        print(bcolors.OKGREEN + 'STARTED BUNDESLIGA BOT' + bcolors.ENDC)
         for team in buli:
             tempTeam = Team(team)
             teams.append(tempTeam)
-            print(tempTeam.aliases)
         bundesliga = Competition(teams)
         return bundesliga
     elif competition == 'cl':
+        print(bcolors.OKGREEN + 'STARTED CHAMPIONSLEAGUE BOT' + bcolors.ENDC)
         for team in clTeams:
             tempTeam = Team(team)
             teams.append(tempTeam)
-            print(tempTeam.aliases)
         cl = Competition(teams)
         return cl
+    else:
+        raise Exception
+
+def chat_id(competition):
+    if competition == 'buli':
+        return secrets.buli_chat_id
+    elif competition == 'cl':
+        return secrets.cl_chat_id
     else:
         raise Exception
