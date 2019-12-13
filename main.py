55b45ffab5640675d33b8cb3945800ed7e1005f8
Felix Schulze Sindern <felixs@fh-muenster.de>
2019-12-13 18:16:49 +0100

    implement functionality to retry scraping max 5 times when scraping wasnt successful

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import telegram
 from datetime import datetime
 from setup import setup
 import sys
-from urllib.error import HTTPError
 from traceback import print_exception
 from telegram.error import BadRequest
 import time
@@ -36,29 +35,33 @@ def main():
 
 def process_submission(submission, bot, competition, chat_id):
     normalized_title = submission.title.lower().split()
-    text = '<a href="{}">{}</a>'.format(submission.url, submission.title)
+    text = f'<a href="{submission.url}">{submission.title}</a>'
     if filter(normalized_title, submission.url, submission.created_utc, competition):
         print(text)
-        try:
-            mp4Link = scrape.mp4Link(submission.url)
-            if mp4Link:
-                print(mp4Link)
-                # TODO: Improve Error handling. When scraping dead links nothin should happen. Example of dead link: https://streamja.com/da6n
-                bot.send_video(chat_id=chat_id, caption=submission.title,
-                               video=mp4Link, timeout=500)
-                print('Successfully scraped mp4 link. Sening video...')
+        def scrape_and_send(submission, bot, chat_id, retries=1):
+            if retries <= 5:
+                try:
+                    mp4Link = scrape.mp4Link(submission.url)
+                    if mp4Link:
+                        print(mp4Link)
+                        # TODO: Improve Error handling. When scraping dead links nothin should happen. Example of dead link: https://streamja.com/da6n
+                        bot.send_video(chat_id=chat_id, caption=submission.title,
+                                    video=mp4Link, timeout=500)
+                        print('Successfully scraped mp4 link. Sening video...')
+                    else:
+                        raise Exception()
+                except BadRequest as e:
+                    time.sleep(5)
+                    print(f'Tried to process dead link (BadRequest) on try No.: {retries} {str(e)}')
+                    scrape_and_send(submission, bot, chat_id, retries+1)
+                except Exception as e:
+                    time.sleep(5)
+                    print(f'This URL {submission.url} caused an Exception in process_submission() on try No.: {retries} {str(e)}')
+                    scrape_and_send(submission, bot, chat_id, retries+1)
             else:
-                print('Couldnt scrape mp4 link. Sening link...')
-                bot.send_message(chat_id=chat_id,
-                                 text=text, parse_mode=telegram.ParseMode.HTML)
-        except BadRequest as e:
-            print('Tried to process dead link (BadRequest): ' + str(e))
-        except Exception as e:
-            print('This URL ' + submission.url + ' caused an Exception in process_submission(): ' + str(e))
-            # bot.send_message(chat_id=chat_id,
-            #                  text='Whoops! Something went wrong when scraping this URL: ' + submission.url)
-            bot.send_message(chat_id=chat_id,
-                             text=text, parse_mode=telegram.ParseMode.HTML)
+                print(f'This URL {submission.url} couldnt be scraped 5 times in a row. Sending the direct link now.')
+                bot.send_message(chat_id=chat_id, text=text, parse_mode=telegram.ParseMode.HTML)
+        scrape_and_send(submission, bot, chat_id)
 
 
 def filter(title, url, date, competition):
--- scrape.py
+++ scrape.py
@@ -3,24 +3,18 @@ from urllib.error import HTTPError
 # TODO: "imgtc", "vimeo"
 
 def mp4Link(url):
-    try:
-        if 'streamja' in url:
-            d = pq(url=url)('video > source')
-            return d.attr('src')
-        if 'streamable' in url:
-            d = pq(url=url)('div > video')
-            return 'http:' + d.attr('src')
-        if 'clippituser' in url:
-            d = pq(url=url)('#player-container')
-            return d.attr('data-hd-file')
-        if 'streamvi' in url:
-            d = pq(url=url)('video > source')
-            return d.attr('src')
-        else:
-            print('No scraping routine specified for this host.')
-            return False
-    except HTTPError as e:
-        print('Tried to process dead link. (HTTPError): ' + str(e))
-    except Exception as e:
-        print('URL: ' + url)
-        print('Exception occured in mp4Link(url): ' + str(e))
+    if 'streamja' in url:
+        d = pq(url=url)('video > source')
+        return d.attr('src')
+    if 'streamable' in url:
+        d = pq(url=url)('div > video')
+        return 'http:' + d.attr('src')
+    if 'clippituser' in url:
+        d = pq(url=url)('#player-container')
+        return d.attr('data-hd-file')
+    if 'streamvi' in url:
+        d = pq(url=url)('video > source')
+        return d.attr('src')
+    else:
+        print('No scraping routine specified for this host.')
+        return False
