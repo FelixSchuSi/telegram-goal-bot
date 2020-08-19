c72c1c8617927ce69b95a6e9380b7991a2c0a3c5
Felix Schulze Sindern <felixs@fh-muenster.de>
2020-08-19 19:58:39 +0200

    Fix wrong error handling.

--- scrape.py
+++ scrape.py
@@ -4,22 +4,18 @@ import requests
 # TODO: "imgtc", "vimeo"
 
 def mp4_link(url):
-    try:
-        if 'streamja' in url:
-            d = pq(url=url)('video > source')
-            return d.attr('src')
-        if 'streamable' in url:
-            d = pq(url=url)('div > video')
-            return 'https:' + d.attr('src')
-        if 'clippituser' in url:
-            d = pq(url=url)('#player-container')
-            return d.attr('data-hd-file')
-        if 'streamvi' in url:
-            d = pq(url=url)('video > source')
-            return d.attr('src')
-        else:
-            print(f'[SCRAPE ERROR] No existing routine for url: {url}')
-            return False
-    except Exception:
-        print(f'[SCRAPE ERROR] Exception when scraping this url: {url}')
+    if 'streamja' in url:
+        d = pq(url=url)('video > source')
+        return d.attr('src')
+    if 'streamable' in url:
+        d = pq(url=url)('div > video')
+        return 'https:' + d.attr('src')
+    if 'clippituser' in url:
+        d = pq(url=url)('#player-container')
+        return d.attr('data-hd-file')
+    if 'streamvi' in url:
+        d = pq(url=url)('video > source')
+        return d.attr('src')
+    else:
+        print(f'[SCRAPE ERROR] No existing routine for url: {url}')
         return False
