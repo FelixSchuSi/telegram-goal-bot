b488e08be5fe2b4779982cac590b55ac2e61dda3
Felix Schulze Sindern <felixs@fh-muenster.de>
2023-09-22 18:03:03 +0200

    fix

--- main.py
+++ main.py
@@ -6,48 +6,74 @@ import time
 
 
 def main():
-  apis = setup()
-  try:
-    # Use this for testing!
-    # submissions = apis["subreddit"].new()
-    # for submission in submissions:
-    #   process_submission(apis, submission)
+    apis: Apis = setup()
+    alternative_angles = AlternativeAngles(apis)
 
-    for submission in apis["subreddit"].stream.submissions():
-      process_submission(apis, submission)
-  except KeyboardInterrupt:
-    print('CTRL + C detected. closing...')
-    quit()
-  except Exception as e:
-    print('Exception in main() occured: ' + str(e))
+    try:
+        # Use this for testing!
+        submissions = apis.subreddit.new()
+        for submission in submissions:
+            process_submission(apis, submission, alternative_angles)
 
-
-def process_submission(apis, submission):
-  passed_filter = filter_submission(submission, apis["competition"])
-  if not passed_filter:
-    return
-  mp4_link = scrape_with_retries(submission.url, submission.title)
-  send_video(apis, submission.title, (submission.url, mp4_link)) if mp4_link else send_message(apis, submission.title,
-                                                                                               submission.url)
+        # for submission in apis["subreddit"].stream.submissions():
+        #   process_submission(apis, submission)
+    except KeyboardInterrupt:
+        print("CTRL + C detected. closing...")
+        quit()
+    except Exception as e:
+        print("Exception in main() occured: " + str(e))
 
 
-def filter_submission(submission, competition):
-  title = submission.title.lower().split()
-  # title must contain a hyphen AND not be a u19 or u21 game.
-  if (any('-' in e for e in title) and not any('u19' in e for e in title) and not any('u21' in e for e in title) and not any('w' in e for e in title)) or len(competition.teams) == 0:
-    # video must be hosted on one of the specified services.
-    if any(host in submission.url for host in
-           ['streamwo', 'streamja', 'streamye', 'streamable', 'imgtc', 'clippituser', 'vimeo', 'streamvi', 'juststream', 'streamff', 'streamgg', 'streamin', 'dubz']):
-      diff = datetime.utcnow() - datetime.utcfromtimestamp(submission.created_utc)
-      # post must be younger than 3 minutes.
-      if (diff.total_seconds() / 60) < 3:
-        # title must contain two teams of the specified competition.
-        if competition.is_competition(title):
-          return True
+def process_submission(
+    apis: Apis, submission: Submission, alternative_angles: AlternativeAngles
+) -> None:
+    passed_filter = filter_submission(submission, apis.competition)
+    if not passed_filter:
+        return
+    mp4_link = scrape_with_retries(submission.url, submission.title)
+    bot_message = (
+        send_video(apis, submission.title, (submission.url, mp4_link))
+        if mp4_link
+        else send_message(apis, submission.title, submission.url)
+    )
+    alternative_angles.register_goal(submission, bot_message)
 
 
-if __name__ == '__main__':
-  while True:
-    main()
-    print('Praw API is down. Restarting in 3 mins...')
-    time.sleep(180)
+def filter_submission(submission, competition: Competition) -> bool:
+    title = submission.title.lower().split()
+    # title must contain a hyphen AND not be a u19 or u21 game.
+    if (
+        any("-" in e for e in title)
+        and not any("u19" in e for e in title)
+        and not any("u21" in e for e in title)
+    ):
+        # video must be hosted on one of the specified services.
+        if any(
+            host in submission.url
+            for host in [
+                "streamwo",
+                "streamja",
+                "streamye",
+                "streamable",
+                "imgtc",
+                "clippituser",
+                "vimeo",
+                "streamvi",
+                "streamin",
+                "dubz",
+            ]
+        ):
+            diff = datetime.utcnow() - datetime.utcfromtimestamp(submission.created_utc)
+            # post must be younger than 3 minutes.
+            # if (diff.total_seconds() / 60) < 3:
+            # title must contain two teams of the specified competition.
+            if competition.is_competition(title):
+                return True
+    return False
+
+
+if __name__ == "__main__":
+    while True:
+        main()
+        print("Praw API is down. Restarting in 3 mins...")
+        time.sleep(180)
--- scrape.py
+++ scrape.py
@@ -7,62 +7,63 @@ import time
 
 
 def mp4_link(url):
-  if 'streamja' in url:
-    d = pq(url=url)('video > source')
-    return d.attr('src')
-  if 'streamable' in url:
-    d = pq(url=url)('div > video')
-    return 'https:' + d.attr('src')
-  if 'clippituser' in url:
-    d = pq(url=url)('#player-container')
-    return d.attr('data-hd-file')
-  if 'streamvi' in url:
-    d = pq(url=url)('video > source')
-    return d.attr('src')
-  if 'streamgg' in url:
-    d = pq(url=url)('video > source')
-    return d.attr('src')
-  if 'dubz' in url:
-    d = pq(url=url)('video')
-    return d.attr('src')
-  if 'streamff' in url:
-    d = pq(url=url)('video')
-    return d.attr('src')
-  if 'streamin' in url:
-    d = pq(url=url)('video')
-    return d.attr('src')
-  if 'streamye' in url:
-    d = pq(url=url)('body video > source')
-    src = d.attr('src')
-    if src.startswith('.'):
-      src = 'https://streamye.com' + src[1:]
-    return src
-  if 'streamwo' in url:
-    d = pq(url=url)('body video > source')
-    src = d.attr('src')
-    if src.startswith('.'):
-      src = 'https://streamwo.com' + src[1:]
-    return src 
-  else:
-    print(f'[SCRAPE ERROR] No existing routine for url: {url}')
-    return False
+    if "streamja" in url:
+        d = pq(url=url)("video > source")
+        return d.attr("src")
+    if "streamable" in url:
+        d = pq(url=url)("div > video")
+        return "https:" + d.attr("src")
+    if "clippituser" in url:
+        d = pq(url=url)("#player-container")
+        return d.attr("data-hd-file")
+    if "streamvi" in url:
+        d = pq(url=url)("video > source")
+        return d.attr("src")
+    if "streamgg" in url:
+        d = pq(url=url)("video > source")
+        return d.attr("src")
+    if "dubz" in url:
+        d = pq(url=url)("video")
+        return d.attr("src")
+    if "streamff" in url:
+        d = pq(url=url)("video")
+        return d.attr("src")
+    if "streamin" in url:
+        d = pq(url=url)("video")
+        return d.attr("src")
+    if "streamye" in url:
+        d = pq(url=url)("body video > source")
+        src = d.attr("src")
+        if src.startswith("."):
+            src = "https://streamye.com" + src[1:]
+        return src
+    if "streamwo" in url:
+        d = pq(url=url)("body video > source")
+        src = d.attr("src")
+        if src.startswith("."):
+            src = "https://streamwo.com" + src[1:]
+        return src
+    else:
+        print(f"[SCRAPE ERROR] No existing routine for url: {url}")
+        return False
 
 
 def scrape_with_retries(url, title, retries=1):
-  if retries <= 5:
-    try:
-      link = mp4_link(url)
-      if not link: return False
-      print('[SUCCESSFULLY SCRAPED]', title, link)
-      return link
-    except BadRequest as e:
-      print('[BAD REQUEST]', title, url, str(e))
-      return False
-    except Exception as e:
-      # In most cases the video is not processed yet. We will try again in a few secs.
-      print(f'[RETRY NO {retries}]', title, url, str(e))
-      time.sleep(15)
-      return scrape_with_retries(url, title, retries + 1)
-  else:
-    print(f'[ERR AFTER {retries} RETRIES]', title, url)
-    return False
+    if retries <= 5:
+        try:
+            link = mp4_link(url)
+            if not link:
+                return False
+            print("[SUCCESSFULLY SCRAPED]", title, link)
+            return link
+        except BadRequest as e:
+            print("[BAD REQUEST]", title, url, str(e))
+            return False
+        except Exception as e:
+            # In most cases the video is not processed yet. We will try again in a few secs.
+            print(f"[RETRY NO {retries}]", title, url, str(e))
+            time.sleep(15)
+            return scrape_with_retries(url, title, retries + 1)
+    else:
+        print(f"[ERR AFTER {retries} RETRIES]", title, url)
+        return False
