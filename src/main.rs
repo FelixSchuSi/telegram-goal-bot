8b6714f29a8d5c0ef9ca5f25329eaeb2aef217f1
Felix Schulze Sindern <f.schulzesindern@web.de>
2026-02-18 20:24:10 +0100

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use crate::reddit::get_fresh_subreddit::get_fresh_subreddit;
+use crate::scrape::get_html_with_browser::get_html_with_browser;
 use chrono::{DateTime, Local};
 use config::config::Config;
 use dotenv::dotenv;
 use filter::competition::CompetitionName;
-use futures_util::future;
 use log::{error, info};
 use reddit::listen_for_submissions::RedditHandle;
 use roux::Subreddit;
@@ -43,6 +43,14 @@ async fn main() {
         .init();
     dotenv().ok();
     error!("successfully read dotenv");
+    let scrape_res = get_html_with_browser("https://streamff.link/v/7b56c7af", "video")
+        .await
+        .map_err(|err| {
+            error!("Error while scraping: {:?}", err);
+            err
+        });
+
+    println!("Scrape result: {:?}", scrape_res);
 
     // List of submissions that are goals and were posted to telegram.
     // We want to listen for comments for this submission to find replays of that goal to post them to telegram as well.
--- src/scrape/mod.rs
+++ src/scrape/mod.rs
@@ -1,4 +1,4 @@
-mod get_html_with_browser;
+pub mod get_html_with_browser;
 mod get_html_without_browser;
 pub mod scrape;
 mod scrape_html;
